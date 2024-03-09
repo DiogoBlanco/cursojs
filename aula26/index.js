6f866627734e63450eb79e63467c876c09721788
DiogoBlanco <diogoblanco@outlook.com.br>
2024-03-09 20:50:44 -0300

    aula 67

---
+++ aula26/index.js
@@ -0,0 +1,12 @@
+let num1 = 1; // number
+let num2 = 2.5; // number
+
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+
+/* Métodos com números */
+
+// toString() - converte para string
+// toFixed(2) - arredonda as casas decimais
+// Number.isInteger(num) - retorna se é inteiro ou não
+//
